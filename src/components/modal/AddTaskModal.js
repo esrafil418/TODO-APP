00208331171b8420426898b27bfe4f92da673a50
esrafil <esrafil.s.r.man418@gmail.com>
2025-11-05 19:03:41 +0330

    feat: Add: persian calender - status feature to AddTaskModal component

--- src/components/modal/AddTaskModal.js
+++ src/components/modal/AddTaskModal.js
@@ -1,16 +1,18 @@
 import { El } from "../../utils/el";
+import "@majidh1/jalalidatepicker/dist/jalalidatepicker.min.css";
+import "@majidh1/jalalidatepicker/dist/jalalidatepicker.min.js";
 
 export function AddTaskModal(onSubmit) {
   const modal = El({
     element: "div",
     id: "taskModal",
     className:
-      "fixed inset-0 bg-black/50 flex justify-center items-center hidden z-50",
+      "fixed inset-0 bg-black/80 flex justify-center items-center hidden z-10",
     children: [
       El({
         element: "div",
         className:
-          "bg-white p-5 rounded-xl w-[500px] h-[300px] flex flex-col gap-3",
+          "bg-white p-5 rounded-xl w-[500px] h-auto flex flex-col gap-3",
         children: [
           El({
             element: "h2",
@@ -35,14 +37,57 @@ export function AddTaskModal(onSubmit) {
                 className:
                   "border border-gray-300 rounded-md p-2 focus:outline-[#6200ea]",
                 children: [
-                  El({ element: "option", value: "low", innerText: "Low" }),
+                  El({
+                    element: "option",
+                    value: "low",
+                    innerText: "Low",
+                  }),
                   El({
                     element: "option",
                     value: "medium",
                     innerText: "Medium",
                   }),
-                  El({ element: "option", value: "high", innerText: "High" }),
+                  El({
+                    element: "option",
+                    value: "high",
+                    innerText: "High",
+                  }),
                 ],
+              }),
+              El({
+                element: "select",
+                id: "status",
+                className:
+                  "border border-gray-300 rounded-md p-2 focus:outline-[#6200ea]",
+                children: [
+                  El({
+                    element: "option",
+                    value: "todo",
+                    innerText: "Todo",
+                  }),
+                  El({
+                    element: "option",
+                    value: "doing",
+                    innerText: "Doing",
+                  }),
+                  El({
+                    element: "option",
+                    value: "done",
+                    innerText: "Done",
+                  }),
+                ],
+              }),
+              El({
+                element: "input",
+                id: "deadline",
+                type: "text",
+                placeholder: "Deadline",
+                dataset: {
+                  jdp: "",
+                  jdpMinDate: "today",
+                },
+                className:
+                  "border border-gray-300 rounded-md p-2 focus:outline-[#6200ea] text-center text-red-700 font-bold",
               }),
               El({
                 element: "button",
@@ -65,19 +110,24 @@ export function AddTaskModal(onSubmit) {
     e.preventDefault();
     const taskName = modal.querySelector("#taskName").value.trim();
     const priority = modal.querySelector("#priority").value;
+    const status = modal.querySelector("#status").value;
+    const deadline = modal.querySelector("#deadline").value;
+
     if (!taskName) return;
 
     const newTask = {
       id: Date.now(),
       taskName,
       priority,
-      status: "todo",
-      deadline: "-",
+      status,
+      deadline,
     };
     onSubmit(newTask);
     modal.hide();
     e.target.reset();
   });
+
+  jalaliDatepicker.startWatch();
 
   return modal;
 }
